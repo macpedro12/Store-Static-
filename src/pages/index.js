5feeb666f9431a0b72f8816ee8e13cbb5e4123e5
macpedro12 <pedroamorim62@gmail.com>
2021-08-11 11:33:45 -0300

    Added new project

---
+++ src/pages/index.js
@@ -0,0 +1,13 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import Header from '../components/Header/header'
+
+
+export default function Home() {
+  return (
+    <div>
+      <Header/>
+      <h1>OI</h1>
+    </div>
+  )
+}
